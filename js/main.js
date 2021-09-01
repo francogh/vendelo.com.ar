fe8fde06fe8a26ac720932fd6fe7fdb41b8be841
franco ghiotti <francoghiotti@gmail.com>
2021-09-01 12:04:57 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// let nombre = prompt('ingrese su nombre');
+
+
+// console.log (nombre);
+
+let nombre = prompt('Ingrese su nombre');
+alert('Hola' + ' ' + nombre);
+
+let edad = prompt('Ingrese su edad');
+let edadInt = parseInt(edad);
+
+let anioActual = 2021;
+
+console.log('Tu nombre es ' + nombre + ' ' +'y naciste en el año' + ' ' + (anioActual - edadInt));
